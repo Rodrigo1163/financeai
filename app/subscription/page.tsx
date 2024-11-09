ff2ca2e467643092f81987fded2b01e5ac06795f
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-11-09 12:52:56 -0400

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,7 @@
+const Subcript = () => {
+    return (
+        <h1>SUbcript page</h1>
+    )
+}
+
+export default Subcript
---
+++ app/transactions/page.tsx
@@ -0,0 +1,7 @@
+export default function Transactions() {
+    return (
+        <>
+            <h1>hellp word</h1>
+        </>
+    )
+}
