5d88fd2e4611d3a95ee1d785f2ef9158b7ee41f2
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-12-07 14:55:42 -0400

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
 import NavBar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
-const SubcriptPage = () => {
+const SubcriptPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
     <div>
       <NavBar />
