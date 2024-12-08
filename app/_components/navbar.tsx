e35191d081af83de389a5240a82fa7e1e14fcd38
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-12-08 15:51:20 -0400

    feat: add expenses per category component

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -5,10 +5,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const NavBar = () => {
+const Navbar = () => {
   const pathname = usePathname();
   return (
     <nav className="flex justify-between border-b border-solid px-8 py-4">
+      {/* ESQUERDA */}
       <div className="flex items-center gap-10">
         <Image src="/logo.svg" width={173} height={39} alt="Finance AI" />
         <Link
@@ -42,10 +43,10 @@ const NavBar = () => {
           Assinatura
         </Link>
       </div>
-
+      {/* DIREITA */}
       <UserButton showName />
     </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
