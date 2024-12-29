e4c79e14af4d5ca95dd51940a31ebc845c075b46
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-12-29 13:49:40 -0400

    fix: ensure user can scroll on transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,7 +2,7 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
-import NavBar from "../_components/navbar";
+import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
@@ -13,7 +13,6 @@ const TransactionsPage = async () => {
   if (!userId) {
     redirect("/login");
   }
-
   const transactions = await db.transaction.findMany({
     where: {
       userId,
@@ -22,26 +21,24 @@ const TransactionsPage = async () => {
       date: "desc",
     },
   });
-  const userCanAddTransactions = await canUserAddTransaction();
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
-    <div>
-      <NavBar />
-      <div className="space-y-6 overflow-hidden p-6">
+    <>
+      <Navbar />
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton
-            userCanAddTransaction={userCanAddTransactions}
-          />
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <ScrollArea>
+        <ScrollArea className="h-full">
           <DataTable
             columns={transactionColumns}
             data={JSON.parse(JSON.stringify(transactions))}
           />
         </ScrollArea>
       </div>
-    </div>
+    </>
   );
 };
 
