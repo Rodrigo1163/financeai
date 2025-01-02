ecf7c0fbe26ace86363000e2f0c7e6eb80c76786
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2025-01-02 01:22:31 -0400

    feat: ensure that the user adds a transaction before generating the report

--- app/(home)/_actions/generate-ai-report/index.ts
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -36,6 +36,9 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
     },
   });
   //mandar as transações para o chatgpt e pedir para ele gerar um relatório com
+  if (transactions.length === 0)
+    return "Adicione uma transação para gerar o relatório.";
+
   const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
   ${transactions
     .map(
